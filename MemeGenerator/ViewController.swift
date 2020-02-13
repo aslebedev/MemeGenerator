fb50ead7eba101c9b0a44099718e2b22e76e0591
Alexander Lebedev <alexander@alebedev.me>
2020-02-13 14:19:31 +0700

    Initial Commit

---
+++ MemeGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeGenerator
+//
+//  Created by alexander on 13.02.2020.
+//  Copyright © 2020 alexander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
